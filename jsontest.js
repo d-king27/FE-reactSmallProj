bea4796690586df6e44aec48c96801e8881eb35f
d-king27 <kingd8883@gmail.com>
2017-10-04 12:06:10 +0100

    implemented a bracket finding function, many complicated functions needed for this

--- jsontest.js
+++ jsontest.js
@@ -1,17 +1,67 @@
-var emoji = require('./src/components/emoji.json')
+var add = function(a,b)
+{return a + b}
+
+var sub = function(a,b)
+{return a - b}
+
+var mult = function(a,b)
+{return a * b}
 
 
-function findWord(word){
-  var Regex = new RegExp(word, 'i')
- var firstChoice = emoji.filter(function(item){
-   return Regex.test(item.title)
-  })
- if(firstChoice.length > 0){return firstChoice}
- else{
-   return emoji.filter(function(item){
-     return Regex.test(item.keywords)
-   })
- }
+var div = function(a,b)
+{return a / b}
+
+var squ = function(a)
+{return a * a}
+
+var math = {
+  '+':add,
+  '-':sub,
+  '*':mult,
+  '/':div,
+  'x2':squ
 }
 
-console.log(findWord('sa'))
+
+
+// function repCalc(str){
+// }
+
+// console.log(repCalc())
+
+
+
+// var regex = /(\d+)([\/*+-])?(\d+)?([\/*+-])?/
+
+// var test = '61*1'.match(regex)
+
+// console.log(test)
+
+// function doSum(arr){
+//  return math[arr[2]](Number(arr[1]),Number(+arr[3]))
+// }
+
+// console.log(doSum(test))
+
+
+
+function extractBrackets(str){
+  var obj = {0:[]}
+  var open = false
+  var count = 0
+splitStr = str.split('')
+splitStr.forEach(function(item){
+  if(item==='('){
+    open = true
+    return}
+  if(item === ')') {
+    open = false
+  count++
+obj[count] = []}
+  if(open === true){obj[count].push(item)}
+ })
+ return obj
+
+}
+
+console.log(extractBrackets('(6+3) * (7+5)'))
